342739c0ee46156d9203debad753f88a97752379
Inguva Sathvik <143580000+sathvikinguva@users.noreply.github.com>
2024-11-26 22:00:19 +0530

    NexusNet

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBzBpTG9GxrW3RvKtWFEtNICLcbe6nzVtc",
+  authDomain: "socialmedia-591fd.firebaseapp.com",
+  projectId: "socialmedia-591fd",
+  storageBucket: "socialmedia-591fd.appspot.com",
+  messagingSenderId: "727651833491",
+  appId: "1:727651833491:web:7281ee8038b32e5c06df66",
+  measurementId: "G-Z0N214HMPK"
+};
+
+firebase.initializeApp(firebaseConfig);
